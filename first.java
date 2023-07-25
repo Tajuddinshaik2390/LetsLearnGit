056d94d085ffce0a837596ddc2874bf5e62dc823
Tajuddin Shaik <tajuddin.shaik239@gmail.com>
2023-07-26 03:30:20 +0530

    first commit

---
+++ first.java
@@ -0,0 +1,16 @@
+public class first{
+    public static void main(String args[]){
+//swap without 3rd variable
+int x=10;
+int y=20;
+System.out.println("x before swap is "+x+'\n'+"y before swap is"+y);
+x=y+x;//30
+y=x-y;//10
+x=x-y;
+System.out.println("x after swap is "+x+'\n'+"y after swap is"+y);
+
+
+
+
+    }
+}
